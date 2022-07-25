42d7bbeea13c762e4a2cc0e1e774bde7064558c6
dilshodwiut <ramatilloyevdilshod7.0@gmail.com>
2022-07-26 00:01:27 +0500

    temporarily commented out server side fetching to deploy on netlify

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -48,18 +48,18 @@ export default Portfolio;
 export const getStaticProps: GetStaticProps = async (context) => {
   // const projects = await import("@/data/projects.json");
   // const projects = await fetch("@/data/projects.json");
-  const technologies = await fetch(process.env.TECHNOLOGIES_URL);
-  const stats = await fetch(process.env.STATS_URL);
-  const projects = await fetch(process.env.PROJECTS_URL);
-  const technologiesData = await technologies.json();
-  const statsData = await stats.json();
-  const projectsData = await projects.json();
+  // const technologies = await fetch(process.env.TECHNOLOGIES_URL);
+  // const stats = await fetch(process.env.STATS_URL);
+  // const projects = await fetch(process.env.PROJECTS_URL);
+  // const technologiesData = await technologies.json();
+  // const statsData = await stats.json();
+  // const projectsData = await projects.json();
 
   return {
     props: {
-      technologies: technologiesData,
-      stats: statsData,
-      projects: projectsData,
+      technologies: [],
+      stats: [],
+      projects: [],
     },
   };
 };
