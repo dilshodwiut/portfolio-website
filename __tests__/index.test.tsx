66326cb1e01d85f7bc568ddef8d6541d077f5423
dilshodwiut <ramatilloyevdilshod7.0@gmail.com>
2022-08-17 14:53:23 +0500

    updated packages, initialized testing environment

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,13 +1,23 @@
+import * as React from "react";
 import { render, screen } from "@testing-library/react";
 import Home from "../pages/index";
 import "@testing-library/jest-dom";
 
 describe("Home", () => {
-  it("renders a heading", () => {
-    render(<Home />);
+  beforeAll(() => {
+    render(
+      React.cloneElement(<Home />, {
+        technologies: [],
+        stats: [],
+        clients: [],
+        projects: [],
+      })
+    );
+  });
 
-    const heading = screen.getByRole("heading", {
-      name: /welcome to next\.js!/i,
+  it("renders a heading", () => {
+    const heading = screen.getByRole("button", {
+      name: /Download CV/i,
     });
 
     expect(heading).toBeInTheDocument();
